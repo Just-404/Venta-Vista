ad1628be158a6973dfe90780945d3f79acbaaf8e
L-M17 <lfelizvictoriano@gmail.com>
2026-04-10 19:26:52 -0400

    Update script source paths in layout.php

--- app/views/layouts/layout.php
+++ app/views/layouts/layout.php
@@ -12,6 +12,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js"></script>
     <link rel="stylesheet" href="<?= BASE_URL ?>public/css/configuracion.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>css/configuracion.css">
+    <script>window.APP_BASE = '<?= BASE_URL ?>';</script>
+
 </head>
 <body>
 <div class="app-wrapper">
@@ -43,6 +45,6 @@
 
 </div>
 
-<script src="<?= BASE_URL ?>public/js/app.js"></script>
+<script src="<?= BASE_URL ?>js/app.js"></script>
 </body>
 </html>
